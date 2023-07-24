4c0626e1a879dbcedd43f0f41cca2895f40e885e
Nikita <grishaev.nk@gmail.com>
2023-07-25 00:06:12 +0200

    Classes for new entities

---
+++ Assets/Scripts/Game/Obstacle.cs
@@ -0,0 +1,9 @@
+﻿namespace Game
+{
+    public class Obstacle: Entity
+    {
+        public Obstacle(int x, int y) : base(x, y)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Trap.cs
@@ -0,0 +1,9 @@
+﻿namespace Game
+{
+    public class Trap: Entity
+    {
+        public Trap(int x, int y) : base(x, y)
+        {
+        }
+    }
+}
