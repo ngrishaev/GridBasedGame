cd9a536243fa22c332081b9d6bf94dfa3aebacee
Nikita <grishaev.nk@gmail.com>
2023-09-26 01:57:45 +0200

    Now can shoot in all directions

--- Assets/Scripts/Game/PlayerAttacker.cs
+++ Assets/Scripts/Game/PlayerAttacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using Game.Entities;
@@ -7,44 +8,48 @@ namespace Game
 {
     public class PlayerAttacker : IPlayerAttacker
     {
+        private const int Damage = 1;
         private readonly Player _player;
         private readonly List<Entity> _entities;
+        private const int _shootRange = 3;
 
         public PlayerAttacker(Player player, List<Entity> entities)
         {
             _player = player;
             _entities = entities;
         }
-        
-        public void ShootLeft()
-        {
-            throw new System.NotImplementedException();
-        }
 
-        public void ShootRight()
+        public void ShootLeft() => Shoot(Direction.Left);
+        public void ShootRight() => Shoot(Direction.Right);
+        public void ShootUp() => Shoot(Direction.Up);
+        public void ShootDown() => Shoot(Direction.Down);
+        
+        private void Shoot(Direction direction)
         {
-            var shootPosition = _player.Position;
-            var shootRange = 3; 
             var target = _entities
-                .Where(e => IsWithinShootRange(e.Position, shootPosition, shootRange))
+                .Where(e => IsWithinShootRange(e.Position, _player.Position, _shootRange, direction))
                 .OfType<IDamageable>()
                 .FirstOrDefault();
-            
-            target?.TakeDamage(1);
-        }
 
-        public void ShootUp()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void ShootDown()
-        {
-            throw new System.NotImplementedException();
+            target?.TakeDamage(Damage);
         }
         
-        // TODO: Abstract X axis
-        private bool IsWithinShootRange(Vector2 position, Vector2 shootPosition, int shootRange) => 
-            position.X > shootPosition.X && position.X < shootPosition.X + shootRange;
+        private bool IsWithinShootRange(Vector2 position, Vector2 shootPosition, int shootRange, Direction direction) =>
+            direction switch {
+                Direction.Right => position.X > shootPosition.X && position.X <= shootPosition.X + shootRange,
+                Direction.Up => position.Y > shootPosition.Y && position.Y <= shootPosition.Y + shootRange,
+                Direction.Down => position.Y < shootPosition.Y && position.Y >= shootPosition.Y - shootRange,
+                Direction.Left => position.X < shootPosition.X && position.X >= shootPosition.X - shootRange,
+                _ => throw new NotImplementedException()
+            };
+
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
     }
 }
