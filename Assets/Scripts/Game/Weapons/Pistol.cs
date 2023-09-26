0087a8692556ac17a2ba8c0e2e12a5e5a85c0986
Nikita <grishaev.nk@gmail.com>
2023-09-26 13:33:04 +0200

    Fix pistol targeting

--- Assets/Scripts/Game/Weapons/Pistol.cs
+++ Assets/Scripts/Game/Weapons/Pistol.cs
@@ -16,10 +16,10 @@ namespace Game.Weapons
 
         private Vector2 Position => _player.Position;
         
-        public Pistol(Player player, List<Entity> _entities)
+        public Pistol(Player player, List<Entity> entities)
         {
             _player = player;
-            this._entities = _entities;
+            _entities = entities;
         }
 
         public bool CanAim(Vector2 target)
@@ -40,7 +40,7 @@ namespace Game.Weapons
         public void Shoot(Direction direction)
         {
             var target = _entities
-                .Where(target => CanAimInDirection(target.Position, direction))
+                .Where(target => IsInDirection(target.Position, direction) && CanAim(target.Position))
                 .OrderBy(target => Vector2.DistanceSquared(Position, target.Position))
                 .OfType<IDamageable>()
                 .FirstOrDefault();
@@ -48,13 +48,11 @@ namespace Game.Weapons
             target?.TakeDamage(Damage);
         }
 
-        private bool CanAimInDirection(Vector2 target, Direction direction) =>
+        private bool IsInDirection(Vector2 target, Direction direction) =>
             direction switch
             {
-                Direction.Right => target.X > Position.X && target.X <= Position.X + ShootRange,
-                Direction.Up => target.Y > Position.Y && target.Y <= Position.Y + ShootRange,
-                Direction.Down => target.Y < Position.Y && target.Y >= Position.Y - ShootRange,
-                Direction.Left => target.X < Position.X && target.X >= Position.X - ShootRange,
+                Direction.Right or Direction.Left => target.Y.Equals(Position.Y),
+                Direction.Up or Direction.Down => target.X.Equals(Position.X),
                 _ => throw new NotImplementedException()
             };
     }
